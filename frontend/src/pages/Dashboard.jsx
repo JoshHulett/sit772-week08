5a9fd8ea936c25bed6bb2f8f01a4fd9af4696eb5
JoshHulett <josh.hulett@outlook.com>
2026-09-17 01:06:21 +1000

    update dashboard message to verify automated CD for production deployment

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -195,7 +195,7 @@ const Dashboard = () => {
           variant="h4"
           fontWeight={600}
         >
-          Dashboard
+          KoalaTech Dashbaord
         </Typography>
 
         <Typography color="text.secondary">
